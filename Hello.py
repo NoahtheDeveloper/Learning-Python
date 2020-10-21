4989c2515f360104e91326dbe1704bb33d1469cf
Noah Brahim <noah.alhandy@gmail.com>
2020-10-21 10:14:12 -0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,21 @@
+# Imported Modules #
+
+import time
+
+
+
+# 10 Seccond Timer
+time.sleep(10)
+print ("Hello World I am Daddy")
+
+# Another 5 Seccond wait
+time.sleep(5)
+print ("Your coding is very Basic Right Now.") 
+#Note: If you are having multiple print lines or having other lines you need to have print (" Insert Text")
+
+
+# Another 15 Seccond wait
+time.sleep(15)
+
+print ("I can now confirm that you can wait an extended ammount of time.")
+
