23fcadab4370bf64377bb82d8964b4f37e9a4ad8
Noah Brahim <noah.alhandy@gmail.com>
2020-10-21 10:09:11 -0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,14 @@
+# Imported Modules #
+
+import time
+
+
+
+# 10 Seccond Timer
+time.sleep(10)
+print ("Hello World I am Daddy")
+
+# Another 5 Seccond wait
+time.sleep(5)
+print ("Your coding is very Basic Right Now.")
+
