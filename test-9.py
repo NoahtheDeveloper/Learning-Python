d65f1ef0867c73917332eb1351af7399ecd85b57
Noah Brahim <noah.alhandy@gmail.com>
2020-10-27 09:41:47 -0700

    Prints PI and a Squared Number

---
+++ test-9.py
@@ -0,0 +1,23 @@
+#This prints PI and a Squared Number
+
+
+# Imported Module is listed below
+import math
+
+
+# What is PI
+x = math.pi
+
+# Prints PI
+print(x)
+print ("PI has now been printed now moving to the next module.")
+
+
+
+
+# This is a function saying math squared. The number inserted is squared.
+x = math.sqrt(64)
+
+# This Prints the squared number.
+print(x)
+print ("64 Squared has now been printed")
