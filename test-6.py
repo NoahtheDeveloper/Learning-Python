88354c691dced7f7e47ec4fcc3e53c9d06108119
Noah Brahim <noah.alhandy@gmail.com>
2020-10-27 09:26:39 -0700

    Variables and Printing them

---
+++ test-6.py
@@ -0,0 +1,15 @@
+#This prints both the X and Y value
+
+x = "Bravo"
+
+# You do not need the " when using numbers
+n = 6
+
+# When using letters you need to add ""
+y = "Going Dark"
+
+
+#Print Commands for all.
+print(x)
+print(n)
+print(y)
