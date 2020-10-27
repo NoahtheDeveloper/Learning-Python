31ff8b43b6c75882f283eb7ccbef5258ba1026a6
Noah Brahim <noah.alhandy@gmail.com>
2020-10-27 09:36:54 -0700

    X Name is a Monkey

---
+++ test-8.py
@@ -0,0 +1,13 @@
+# Prints after each name
+
+def my_function(fname):
+  print(fname + " is a monkey.")
+
+# Name Function
+my_function("Joe")
+
+# Name Function
+my_function("Jim")
+
+# Name Function
+my_function("Jacob")
